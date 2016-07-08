0ae0337deff13f23c36c6fbb232726ada37a43c1
gRAVEN <stefan.danielsson.1989@gmail.com>
2016-07-08 10:15:27 +0200

    Added GET WALL-type to message types.

--- SimpleSocialNetwork/SharedResources/TcpNetworking.cs
+++ SimpleSocialNetwork/SharedResources/TcpNetworking.cs
@@ -107,14 +107,15 @@ namespace SharedResources
         public const int LOGOUT = 3;
         public const int GET_USERS = 4;
         public const int ADD_FRIEND = 5;
-        public const int GET_FRIENDS_STATUS = 6;
+        public const int GET_FRIEND_STATUS = 6;
         public const int GET_CLIENT_DATA = 7;
         public const int SEND_MESSAGE = 8;
+        private const int GET_WALL = 9;
         
 
         //message types:
-        public const int REQUEST = 9;
-        public const int REPLY = 10;
+        public const int REQUEST = 10;
+        public const int REPLY = 11;
 
         public const int INVALID = -1;
 
@@ -133,11 +134,12 @@ namespace SharedResources
                 case 3: return "LOGOUT";
                 case 4: return "GET USERS";
                 case 5: return "ADD FRIEND";
-                case 6: return "GET FRIENDS STATUS";
+                case 6: return "GET FRIEND STATUS";
                 case 7: return "GET CLIENT DATA";
                 case 8: return "SEND MESSAGE";
-                case 9: return "REQUEST";
-                case 10: return "REPLY";
+                case 9: return "GET_WALL";
+                case 10: return "REQUEST";
+                case 11: return "REPLY";
                 default: return "INVALID";
             }
         }
