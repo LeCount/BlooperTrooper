4a1318b52bf10d78b0de0e89cf5e86adf56368ff
Greven Home <stefan.danielsson.1989@gmail.com>
2016-07-13 01:14:24 +0200

    Added classes for representing the "data" field in TCP messages for all types of requests and responses.

--- SimpleSocialNetwork/WpfClient/App.xaml.cs
+++ SimpleSocialNetwork/WpfClient/App.xaml.cs
@@ -148,7 +148,7 @@ namespace WpfClient
                 case TcpConst.GET_CLIENT_DATA:
 
                     break;
-                case TcpConst.SEND_MESSAGE:
+                case TcpConst.CHAT:
 
                     break;
                 default: break;
