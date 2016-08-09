d5a58023e5681f526a13be0f6ac4d84c1f1c657e
gRAVEN <stefan.danielsson.1989@gmail.com>
2016-08-09 11:30:56 +0200

    Removed implementation for add friend on server db. Aumes ppl allways are not friends at the moment.

--- SimpleSocialNetwork/Server/SQLiteDB.cs
+++ SimpleSocialNetwork/Server/SQLiteDB.cs
@@ -235,27 +235,6 @@ namespace Program
 
         internal bool CheckFriendStatus(string username1, string username2)
         {
-            query = new SQLiteCommand();
-            query.Connection = DBconnection;
-
-            SQLiteParameter param0 = new SQLiteParameter("@USER1", DbType.String) { Value = username1 };
-            SQLiteParameter param1 = new SQLiteParameter("@USER2", DbType.String) { Value = username2 };
-
-            query.Parameters.Add(param0);
-            query.Parameters.Add(param1);
-
-            query.CommandText = "SELECT count(*) FROM Relation WHERE (id_user1 = @USER1 AND id_user2 = @USER2) OR (id_user2 = @USER1 AND id_user1 = @USER2)";
-
-            object obj = query.ExecuteScalar();
-            int occurrences = Convert.ToInt32(obj);
-
-            if (occurrences > 0)
-                return true;
-            else
-            {
-
-            }
-
             return false;
         }
 
